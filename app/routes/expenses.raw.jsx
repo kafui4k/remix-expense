6bdf8828ed55c976b939e80f690cfd15fc82df0a
Kafui Alordo <kalordo7@gmail.com>
2023-03-14 17:12:23 +0000

    add a resource route

--- app/routes/expenses.raw.jsx
+++ app/routes/expenses.raw.jsx
@@ -1,3 +1,17 @@
-export default function ExpensesRawPage() {
-  return <h1>ExpensesRaw Page</h1>;
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "First Expense",
+    amount: 12.99,
+    date: new Date().toISOString(),
+  },
+  {
+    id: "e2",
+    title: "Second Expense",
+    amount: 212.99,
+    date: new Date().toISOString(),
+  },
+];
+export function loader() {
+  return DUMMY_EXPENSES;
 }
