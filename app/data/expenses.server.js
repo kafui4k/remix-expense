f6afe32d53889b306437fd07931ac5556f9a4344
Kafui Alordo <kalordo7@gmail.com>
2023-03-15 20:20:47 +0000

    update data

--- app/data/expenses.server.js
+++ app/data/expenses.server.js
@@ -42,3 +42,21 @@ export async function getExpense(id) {
     throw error;
   }
 }
+
+export async function updateExpense(id, expenseData) {
+  try {
+    await prisma.expense.update({
+      where: {
+        id,
+      },
+      data: {
+        title: expenseData.title,
+        amount: +expenseData.amount,
+        date: new Date(expenseData.date),
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
--- app/routes/__app/expenses/$id.jsx
+++ app/routes/__app/expenses/$id.jsx
@@ -1,6 +1,9 @@
 import { useNavigate } from "@remix-run/react";
 import ExpenseForm from "../../../components/expenses/ExpenseForm";
 import Modal from "../../../components/util/Modal";
+import { updateExpense } from "../../../data/expenses.server";
+import { redirect } from "@remix-run/node";
+import { validateExpenseInput } from "../../../data/validation.server";
 // import { getExpense } from "../../../data/expenses.server";
 
 export default function UpdateExpensesAddPage() {
@@ -23,3 +26,18 @@ export default function UpdateExpensesAddPage() {
 //   const expense = await getExpense(expenseId);
 //   return expense;
 // }
+
+export async function action({ params, request }) {
+  const expenseId = params.id;
+  const formData = await request.formData();
+  const expenseData = Object.fromEntries(formData);
+
+  try {
+    validateExpenseInput(expenseData);
+  } catch (error) {
+    return error;
+  }
+
+  await updateExpense(expenseId, expenseData);
+  redirect("/expenses");
+}
