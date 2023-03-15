9eb922fe31042bb0fe3d84f6690d44cd708e2cfd
Kafui Alordo <kalordo7@gmail.com>
2023-03-15 20:52:00 +0000

    extract search parameters and add a visual feedback to the authentication form

--- app/routes/__marketing/auth.jsx
+++ app/routes/__marketing/auth.jsx
@@ -1,9 +1,22 @@
 import AuthForm from "../../components/auth/AuthForm";
-
 import authStyles from "../../styles/auth.css";
 
 export default function AuthPage() {
   return <AuthForm />;
+}
+
+export async function action({ request }) {
+  const searchParams = new URL(request.url).searchParams;
+  const authMode = searchParams.get("mode") || "login";
+
+  const formData = await request.formData();
+  const credentials = Object.fromEntries(formData);
+
+  if (authMode === "login") {
+    //TODO:: login logic
+  } else {
+    // TODO:: sign up logic
+  }
 }
 
 export function links() {
