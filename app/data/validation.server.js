113f2ab987cb61cace5f5fb842d57d556c3cfa0a
Kafui Alordo <kalordo7@gmail.com>
2023-03-15 17:30:50 +0000

    add server side validation

---
+++ app/data/validation.server.js
@@ -0,0 +1,39 @@
+function isValidTitle(value) {
+  return value && value.trim().length() > 0 && value.trim().length <= 30;
+}
+
+function isValidAmount(value) {
+  const amount = parseFloat(value);
+  return !isNaN(amount) && amount > 0;
+}
+
+function isValidDate(value) {
+  return value && new Date(value).getTime() < new Date().getTime();
+}
+
+export function validateExpenseInput(input) {
+  let validationErrors = {};
+
+  if (!isValidTitle(input.title)) {
+    validationErrors.title =
+      "Invalid expense title. Must be at most 30 characters long.";
+  }
+
+  if (!isValidAmount(input.amount)) {
+    validationErrors.amount =
+      "Invalid expense title. Must be at most 30 characters long.";
+  }
+
+  if (!isValidTitle(input.title)) {
+    validationErrors.title =
+      "Invalid amount. Must be a number greater than zero.";
+  }
+
+  if (!isValidDate(input.date)) {
+    validationErrors.date = "Invalid date. Must be a date before today.";
+  }
+
+  if (Object.keys(validationErrors).length > 0) {
+    throw validationErrors;
+  }
+}
--- app/routes/__app/expenses/add.jsx
+++ app/routes/__app/expenses/add.jsx
@@ -4,6 +4,7 @@ import Modal from "../../../components/util/Modal";
 
 import { addExpense } from "../../../data/expenses.server";
 import { redirect } from "@remix-run/node";
+import { validateExpenseInput } from "../../../data/validation.server";
 
 export default function ExpensesAddPage() {
   const navigate = useNavigate();
@@ -23,7 +24,12 @@ export async function action({ request }) {
   const formData = await request.formData();
   const expenseData = Object.fromEntries(formData);
 
-  await addExpense(expenseData);
+  try {
+    validateExpenseInput(expenseData);
+  } catch (error) {
+    return error;
+  }
 
+  await addExpense(expenseData);
   return redirect("/expenses");
 }
